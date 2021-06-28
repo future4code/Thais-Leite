8f117d7dc07e61accb294c74bb10c19ff6bdc101
Thais Pasotti Leite <thaispasottileite@gmail.com>
2021-06-28 14:26:58 -0300

    Exercicio 5 - revisao JS

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -48,7 +48,7 @@ function retornaMaiorNumero(array) { //Cria uma função para retornar maior nú
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
-
+return array.length
 }
 
 // EXERCÍCIO 06
